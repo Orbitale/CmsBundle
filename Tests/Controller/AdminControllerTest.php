f88128d363490cd6a96bffc0a85fd295cd10840e
Alex "Pierstoval" Ancelet <pierstoval@gmail.com>
2015-03-17 12:28:27 +0100

    Moved the skipping test to the concerned test class

--- Tests/Controller/AdminControllerTest.php
+++ Tests/Controller/AdminControllerTest.php
@@ -15,6 +15,23 @@ use Orbitale\Bundle\CmsBundle\Tests\Fixtures\AbstractTestCase;
 class AdminControllerTest extends AbstractTestCase
 {
 
+    public function setUp()
+    {
+        parent::setUp();
+        // Check security context, because of deprecation
+        try {
+            $this->getKernel()->getContainer()->get('security.context');
+        } catch (\Exception $baseException) {
+            $e = $baseException;
+            do {
+                if ($e instanceof \PHPUnit_Framework_Error_Deprecated) {
+                    $this->markTestSkipped('Skip this error while Symfony SecurityContext is used by Twig\'s AppVariable.');
+                }
+                $e = $e->getPrevious();
+            } while ($e);
+        }
+    }
+
     public function testIndex()
     {
         $client = static::createClient();

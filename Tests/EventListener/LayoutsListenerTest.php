b016c781d06a09a4da4f51c4e16e303f70d1a942
maciej.wagner <maciej.wagner@otodom.pl>
2019-08-19 16:42:47 +0200

    Fix tests bug

--- Tests/EventListener/LayoutsListenerTest.php
+++ Tests/EventListener/LayoutsListenerTest.php
@@ -41,8 +41,8 @@ class LayoutsListenerTest extends AbstractTestCase
     }
 
     /**
-     * @expectedException \Twig_Error_Loader
-     * @expectedExceptionMessage Unable to find template this_layout_does_not_exist.html.twig for layout front. The "layout" parameter must be a valid twig view to be used as a layout.
+     * @expectedException \Twig\Error\LoaderError
+     * @expectedExceptionMessage Unable to find template this_layout_does_not_exist.html.twig for layout front. The "layout" parameter must be a valid twig view to be used as a layout in "this_layout_does_not_exist.html.twig".
      */
     public function testLayoutWrong()
     {
